bfaa7b18db7f713e65b3359c0c359971041d52d4
elenamoglan <70642076+elenamoglan@users.noreply.github.com>
2020-10-22 23:08:33 +0300

    Update Problema1_WHILE.py

--- Problema1_WHILE.py
+++ Problema1_WHILE.py
@@ -1,6 +1,6 @@
 #Se introduc succesiv numere nenule până la introducerea numărului 0. Să se afişeze suma tuturor numerelor introduse. 
 suma=0
-x=1
+x=eval(input('Introduceti numarul: '))
 while x!=0:
     suma+=x
     x = eval(input('Introduceti numarul: '))
